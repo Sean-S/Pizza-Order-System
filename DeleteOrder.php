6518786d70f983e7f345295f81b532c5d33aca04
Sean-S <sean.sexton@mycit.ie>
2018-01-17 18:06:33 +0000

    Update DeleteOrder.php

--- DeleteOrder.php
+++ DeleteOrder.php
@@ -1,5 +1,5 @@
 <html>
-	<head>
+<head>
 	<title>Delete Order</title>
 	<meta charset ="utf-8" />
 </head>
@@ -7,8 +7,35 @@
 <body>
     
 <?php
+// connect to database
+include "inc_dbconnect.php";
 
+// user has clicked submit button on Delete Order
+if (isset($_POST['order_id']))
+{
+	$order_id = htmlspecialchars($_POST['order_id']);
+	echo "Order ID: " . $order_id . "<br/>";
+	echo "Test1";
+}
+//user has clicked update hyperlink on Delete Order
+else if (isset($_GET['order_id']))
+{
+	$order_id = htmlspecialchars($_GET['order_id']);
+	echo "Order ID: " . $order_id . "<br/>";
+}
+ 
+// Attempt delete query execution
+$sql = "DELETE FROM orders WHERE order_id = '$order_id'";
+if(mysqli_query($DBConnection, $sql)){
+    echo "Records were deleted successfully. <br/>";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($DBConnection);
+}
+ 
+// Close connection
+mysqli_close($DBConnection);
 
 ?>
+<a href="ViewOrder.php">View Orders</a>		
 </body>
 </html>

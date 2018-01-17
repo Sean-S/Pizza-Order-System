2cbcb85ff155b5a6f734f144368a34fd0d4e33ee
Sean-S <sean.sexton@mycit.ie>
2018-01-17 18:13:26 +0000

    Update inc_DBConnect.php

--- inc_DBConnect.php
+++ inc_DBConnect.php
@@ -1,6 +1,31 @@
 <?php
 
 // connect and select MYySQL pizza database
-	 
-	 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$DBConnection = mysqli_connect($servername, $username, $password);
+
+if ($DBConnection === FALSE)
+{
+	$error = "<p>Unable to connect to the database server.</p>\n";
+	echo $error;
+	exit;
+}
+else
+{
+	//sucessful connection to dbms
+	$DBName = "pizza";
+	$TableName = "orders";
+	//select db in dbms
+	if (!mysqli_select_db($DBConnection,$DBName))
+	{
+		$error = "<p>Unable to select the $DBName database!</p>";
+		echo $error;
+		exit;
+	}
+	echo "DEBUG PRINT : Database connection established.<br>";
+}
+
 ?>
